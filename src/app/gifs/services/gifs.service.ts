34ffae300c190ef98dbe78c0f39eaca64f2d0f88
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-07-23 13:16:16 -0500

    Se establecio el interfaz para el API de Gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_API_KEY = 'hHFDjXGDMfRpA7ikzRvoplXQFwK7zCK7';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'hHFDjXGDMfRpA7ikzRvoplXQFwK7zCK7';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(private http:HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -27,6 +35,20 @@ export class GifsService {
   searchTag( tag: string ):void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit',10)
+      .set('q',tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
+    .subscribe( resp => {
+      this.gifList = resp.data;
+      console.log({ gifs: this.gifList})
+    })
+
+    //const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=hHFDjXGDMfRpA7ikzRvoplXQFwK7zCK7&q=valorant&limit=10')
+
   }
 
 }
