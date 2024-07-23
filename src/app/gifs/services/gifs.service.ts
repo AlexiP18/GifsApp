d78c077d843d526c05c068d4541170ad8f53384b
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-07-23 12:05:57 -0500

    Se establecieron las validaciones de control en el servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,9 +13,20 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag:string){
+    tag = tag.toLowerCase();
+
+    if( this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift( tag );
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+  }
+
   searchTag( tag: string ):void {
-    this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory)
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
   }
 
 }
