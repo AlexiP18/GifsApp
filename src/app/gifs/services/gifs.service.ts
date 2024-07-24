e163c872ea1e7540137afe1e6fd0c7d6a16d09d4
Alexis Poaquiza <apoaquiza6287@uta.edu.ec>
2024-07-23 19:16:33 -0500

    Se establecio para poder visualizar la imagen del gif, el historial y tambien la persistencia del local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,10 @@ export class GifsService {
   private apiKey: string = 'hHFDjXGDMfRpA7ikzRvoplXQFwK7zCK7';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready')
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -30,6 +33,19 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this.tagsHistory.splice(0,10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage():void{
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void{
+    if( localStorage.getItem('history')) return;
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+    if(this._tagsHistory.length === 0) return;
+    this.searchTag(this._tagsHistory[0])
   }
 
   searchTag( tag: string ):void {
